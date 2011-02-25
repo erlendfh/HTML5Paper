4ccae13885356c352d2b59a3614c925034c1e646
Erlend Halvorsen <erlend.halvorsen@aftenposten.no>
2011-02-25 14:57:30 +0100

    page break WIP

--- public/javascripts/ArticleView.js
+++ public/javascripts/ArticleView.js
@@ -16,8 +16,8 @@ Ext.ArticleView = Ext.extend(Ext.Carousel, {
       if (completed) return;
       completed = true;
       var self = this;
-      
-      console.log("size", this.body.getSize());
+      var size = this.body.getSize();
+      console.log(size);
       var firstPage = $(this.body.dom).find(".x-carousel-item:first");
       var ps = firstPage.find("p");
         
@@ -35,22 +35,25 @@ Ext.ArticleView = Ext.extend(Ext.Carousel, {
           columnLeft = pos.left;
 
           if (columnIndex % self.columns == 0) {
+            currentPage[currentPage.length-1] = self.splitParagraph(currentPage[currentPage.length-1], size.height);
+            
             currentPage = [];
             pages.push(currentPage);
           }
         }
-                
-        currentPage.push(this);
+        
+        currentPage.push(p);
       });
-      
-      console.log("pages", pages);
-      
+            
       var self = this;
       $(pages).each(function () {
-        console.log($(this).outer());
+        var html = "";
+        $(this).each(function () {
+          html += $(this).outer();
+        });
         self.add({
-          html: $(this).outer()
-        });        
+          html: html
+        });
       });
       
       self.doLayout();
@@ -65,6 +68,35 @@ Ext.ArticleView = Ext.extend(Ext.Carousel, {
     Ext.ArticleView.superclass.initComponent.call(this);
   },
   
+  splitParagraph: function (p, maxOffset) {
+    var text = this.splitNode(p);
+    var newP = $(text);
+    p.replaceWith(newP);
+
+    newP.find("span").each(function () {
+      console.log(maxOffset, this.offsetTop, this.innerHTML);
+    });
+    
+    return newP;
+  },
+  
+
+  splitNode: function (node) {
+    var self = this;
+      node = $(node);
+      var childNodes = node.attr("childNodes");
+      if (childNodes) {
+          var text = "";
+          $(childNodes).each(function () {
+              text += self.splitNode(this);
+          });
+          node.html(text);
+          return node.outer();
+      } else {
+          return node.text().replace(/([^\s]+)/g, "<span>$1</span>");
+      }
+  },
+    
   formatBody: function(text) {
     return "<p>" + text.replace(/\n\n/g, "</p><p>") + "</p>";
   }
