162ff35c18a973a85d560be5af59f2bd339be4d2
Erlend Halvorsen <erlendfh@gmail.com>
2011-03-04 20:01:18 +0100

    Fixed safeClick on touch devices

--- public/javascripts/safeclick.js
+++ public/javascripts/safeclick.js
@@ -1,17 +1,39 @@
 (function ($) {
   var downPos = {};
-
+  var curPos = {};
   $.fn.safeClick = function (callback) {
+    $(this).bind('touchmove', function (e) {
+      var oe = e.originalEvent.targetTouches[0];
 
-    $(this).mousedown(function (e) {
+      curPos.x = e.pageX;
+      curPos.y = e.pageY;      
+    });
+
+    $(this).bind('mousedown touchstart', function (e) {
+      try {
+        var oe = e.originalEvent.targetTouches[0];
+        if (oe) e = oe;
+      } catch(ex) {}
+      if (!e.pageX) alert("x:" + e.pageX);
       downPos.x = e.pageX;
       downPos.y = e.pageY;
+      curPos.x = e.pageX;
+      curPos.y = e.pageY;
+      console.log("mousedown");
     });
     
-    $(this).mouseup(function (e) {
-      var dx = (e.pageX - downPos.x);
-      var dy = (e.pageY - downPos.y);
+    $(this).bind('mouseup touchend', function (e) {
+      if (e.pageX || e.pageY) {
+        curPos.x = e.pageX;
+        curPos.y = e.pageY;
+      }
+      
+      var dx = (curPos.x - downPos.x);
+      var dy = (curPos.y - downPos.y);
+
       var length = Math.sqrt(dx*dx + dy*dy);
+      curPos = {};
+      downPos = {};
       if (length > 5) { // Ignore drags
         return;
       }
@@ -19,5 +41,15 @@
       callback.apply(this, arguments);
     });
     
+  }
+  
+  function isTouchDevice() {
+     var el = document.createElement('div');
+     el.setAttribute('ongesturestart', 'return;');
+     if(typeof el.ongesturestart == "function"){
+        return true;
+     }else {
+        return false
+     }
   }
 })(jQuery);
